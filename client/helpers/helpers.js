a418be7cf7a35d4d75709d2bc00612366b7eac71
axelzcramer <axelzcramer@gmail.com>
2015-12-05 16:30:29 -0800

    Accounts info data hookups yo

---
+++ client/helpers/helpers.js
@@ -0,0 +1,23 @@
+Template.registerHelper('firstName', function() {
+    return Meteor.user().profile.first_name;
+});
+
+Template.registerHelper('lastName', function() {
+    return Meteor.user().profile.last_name;
+});
+
+Template.registerHelper('email', function() {
+    return Meteor.user().emails[0].address;
+});
+
+Template.registerHelper('phone', function() {
+    return Meteor.user().profile.phone;
+});
+
+Template.registerHelper('primaryChapter', function() {
+    return Meteor.user().profile.primary_chapter;
+});
+
+Template.registerHelper('bio', function() {
+    return Meteor.user().profile.bio;
+});
