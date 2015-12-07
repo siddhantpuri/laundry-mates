20f8ed7a5b1db6031ef70ba5c91d14ff36e44e44
axelzcramer <axelzcramer@gmail.com>
2015-12-06 19:56:46 -0800

    Mobile navbar added. Desktop Navbar perfected. Footer created.

--- client/templates/includes/tlnav.js
+++ client/templates/includes/tlnav.js
@@ -1,10 +1,52 @@
+/* MOBILE NAV EVENTS
+---------------------------------*/
+Template.tlnav.events({
+	"click #icon-menu-img": function(event){
+		$('.mobile-tl-nav').toggleClass('display-none');
+		$('.mobile-icon-menu').toggleClass('big-margin-right')
+	},
+	"click .mob-folder-wrapper-login": function(event){
+		$('.folder-child-login').toggleClass('display-none');
+		$('.folder-child-tl-now, .folder-child-about, .folder-child-be-involved, .folder-child-initiatives, .folder-child-chapters').addClass('display-none');
+	},
+	"click .mob-folder-wrapper-chapters": function(event){
+		$('.folder-child-chapters').toggleClass('display-none');
+		$('.folder-child-tl-now, .folder-child-login, .folder-child-be-involved, .folder-child-initiatives, .folder-child-about').addClass('display-none');
+	},
+	"click .mob-folder-wrapper-tl-now": function(event){
+		$('.folder-child-tl-now').toggleClass('display-none');
+		$('.folder-child-chapters, .folder-child-login, .folder-child-be-involved, .folder-child-initiatives, .folder-child-about').addClass('display-none');
+	},
+	"click .mob-folder-wrapper-be-involved": function(event){
+		$('.folder-child-be-involved').toggleClass('display-none');
+		$('.folder-child-tl-now, .folder-child-login, .folder-child-chapters, .folder-child-initiatives, .folder-child-about').addClass('display-none');
+	},
+	"click .mob-folder-wrapper-initiatives": function(event){
+		$('.folder-child-initiatives').toggleClass('display-none');
+		$('.folder-child-tl-now, .folder-child-login, .folder-child-be-involved, .folder-child-chapters, .folder-child-about').addClass('display-none');
+	},
+	"click .mob-folder-wrapper-about": function(event){
+		$('.folder-child-about').toggleClass('display-none');
+		$('.folder-child-tl-now, .folder-child-login, .folder-child-be-involved, .folder-child-initiatives, .folder-child-chapters').addClass('display-none');
+	},
+});
+
+/* DESKTOP NAV EVENTS
+--------------------------*/
+Template.tlnav.events({
+	"click #nav-login-button": function(event){
+		$('.folder-child-login').toggleClass('display-none');
+		$('#nav-login-button').toggleClass('grey');
+	}, 
+});
+
 Template.tlnav.events({
 	"mouseenter .folder-wrapper-chapters": function(event){
 		$('.folder-child-chapters').removeClass('display-none')
 	}, 
 	"mouseleave .folder-wrapper-chapters": function(event){
 		$('.folder-child-chapters').addClass('display-none')
-	},
+	}
 });
 
 Template.tlnav.events({
