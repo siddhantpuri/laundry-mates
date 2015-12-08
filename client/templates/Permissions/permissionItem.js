01d8869636eef577eaa94ae3f6ab6eb15d6f8e16
Shaan Appel <shaanappel@u1-irc-78-110.ResHall.Berkeley.EDU>
2015-12-08 12:08:25 -0800

    Permissions funcitonal for all roles

--- client/templates/Permissions/permissionItem.js
+++ client/templates/Permissions/permissionItem.js
@@ -3,9 +3,10 @@ Template.permissionItem.events({
 	"click .approve-btn": function(event) {
 		var request_userId = Requests.findOne(this._id).request_userId;
 		var chapter = Requests.findOne(this._id).request_chapter;
+		var new_role = Requests.findOne(this._id).type;
 		
 		var updated_profile = Meteor.users.findOne(request_userId).profile.role;
-		updated_profile[chapter] = "host";
+		updated_profile[chapter] = new_role;
 		Requests.update(this._id, {$set: {request_approved: "true"}});
 		Meteor.users.update(request_userId, {$set: {"profile.role": updated_profile}});
 		console.log(Meteor.users.findOne(request_userId).profile.role)
@@ -19,8 +20,19 @@ Template.permissionItem.events({
 	"click .unapprove-btn": function(event) {
 		var request_userId = Requests.findOne(this._id).request_userId;
 		var chapter = Requests.findOne(this._id).request_chapter;
+		var new_role = Requests.findOne(this._id).type;
+
 		var updated_profile = Meteor.users.findOne(request_userId).profile.role;
-		updated_profile[chapter] = "lounger";
+		if (new_role == "superadmin") {
+			updated_profile[chapter] = "admin";
+		} else {
+			if (new_role == "admin") {
+				updated_profile[chapter] = "host";
+			} else {
+				updated_profile[chapter] = "lounger";
+			}
+		}
+
 		Requests.update(this._id, {$set: {request_approved: "false"}});
 		Meteor.users.update(request_userId, {$set: {"profile.role": updated_profile}});
 		console.log(Meteor.users.findOne(request_userId).profile.role)
