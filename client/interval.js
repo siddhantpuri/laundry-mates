56044d4f8df1661672f5d9dce9d9a7d48ad16451
Shaan Appel <shaanappel@Shaans-MacBook-Pro.local>
2016-01-09 00:25:08 -0500

    reminder emails

--- client/interval.js
+++ client/interval.js
@@ -20,35 +20,70 @@ var intervalId = Meteor.setInterval( function () {
             Lounges.update({_id: lounge._id}, { $set: {lounge_host_sent: "sent"}});
         });
 
-        var notif_date = new Date();
-        notif_date.setDate(notif_date.getDate()+7);
-        var soon_lounges = Lounges.find({lounge_date: {$gt: currentdate, $lt: notif_date}, lounge_reminders_sent: {$exists : false }}, {sort: { lounge_date: -1 }});
-    	soon_lounges.forEach(function(lounge) {
-            var participants = lounge.lounge_participants;
-        
-            for (i = 1; i < participants.length; i++) {
-                var user = Meteor.users.findOne({_id: participants[i]});
-                if (user) {
-                    email = user.emails[0].address;
+        var notif_dates = {first: 7, second: 4, third: 2, fourth: 1}
+
+        var x;
+        for (x in notif_dates) {
+            var value = notif_dates[x];
+            var upper_bound = new Date();
+            upper_bound.setDate(upper_bound.getDate()+value);
+            var lower_bound = new Date();
+            lower_bound.setDate(lower_bound.getDate()+value-1);
+            if (value == 7) {
+                var soon_lounges = Lounges.find({lounge_date: {$gt: lower_bound, $lt: upper_bound}, lounge_reminders_sent: {$exists : false }}, {sort: { lounge_date: -1 }});
+            } else {
+                if (value == 4) {
+                    var soon_lounges = Lounges.find({lounge_date: {$gt: lower_bound, $lt: upper_bound}, lounge_reminders_sent4: {$exists : false }}, {sort: { lounge_date: -1 }});
                 } else {
-                    email = participants[i];
+                    if (value == 2){
+                        var soon_lounges = Lounges.find({lounge_date: {$gt: lower_bound, $lt: upper_bound}, lounge_reminders_sent2: {$exists : false }}, {sort: { lounge_date: -1 }});
+                    } else {
+                        var soon_lounges = Lounges.find({lounge_date: {$gt: lower_bound, $lt: upper_bound}, lounge_reminders_sent1: {$exists : false }}, {sort: { lounge_date: -1 }});
+                    }
                 }
-                var h_data = {
-                day: lounge.lounge_day,
-                date: lounge.lounge_date_numbers,
-                time: lounge.lounge_time,
-                location: lounge.lounge_location,
-                address: lounge.lounge_address
-                }            
-                var h_to = email;
-                var h_subject = "[Reminder] You have an upcoming TL Session in less than 7 days.";
-                var temp_name = 'loungeReminderEmail';
-                var file_name = '7_day_reminder.html';
-                Meteor.call('sendEmail', h_to, h_subject, h_data,  temp_name, file_name);
-                console.log('sent');
             }
-            Lounges.update({_id: lounge._id}, { $set: {lounge_reminders_sent: "sent"}});
-        });
+            soon_lounges.forEach(function(lounge) {
+                var participants = lounge.lounge_participants;
+            
+                for (i = 1; i < participants.length; i++) {
+                    var user = Meteor.users.findOne({_id: participants[i]});
+                    if (user) {
+                        email = user.emails[0].address;
+                    } else {
+                        email = participants[i];
+                    }
+                    var h_data = {
+                    num_days: ""+value,
+                    day: lounge.lounge_day,
+                    date: lounge.lounge_date_numbers,
+                    time: lounge.lounge_time,
+                    location: lounge.lounge_location,
+                    address: lounge.lounge_address
+                    }            
+                    var h_to = email;
+                    var h_subject = "[Reminder] You have an upcoming TL Session in less than "+ value +" days.";
+                    var temp_name = 'loungeReminderEmail';
+                    var file_name = '7_day_reminder.html';
+                    Meteor.call('sendEmail', h_to, h_subject, h_data,  temp_name, file_name);
+                    console.log('sent');
+                }
+               
+                if (value == 7) {
+                    Lounges.update({_id: lounge._id}, { $set: {lounge_reminders_sent: "sent"}});
+                } else {
+                    if (value == 4) {
+                        Lounges.update({_id: lounge._id}, { $set: {lounge_reminders_sent4: "sent"}});
+                    } else {
+                        if (value == 2){
+                            Lounges.update({_id: lounge._id}, { $set: {lounge_reminders_sent2: "sent"}});
+                        } else {
+                            Lounges.update({_id: lounge._id}, { $set: {lounge_reminders_sent1: "sent"}});
+                        }
+                    }
+                }
+            });
+
+        }
 
         console.log('ran')
 }, 1000 );
